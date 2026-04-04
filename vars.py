f2a69e9ec849e424d75252d5d681f47fd1348865
Kamlesh Baldaniya <121075066+kamlesh1629@users.noreply.github.com>
2026-04-04 12:24:16 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
-auth_users = [1713924419]
-sudo_user = 1713924419
-log_channel = -1002143629860
-txt_channel = -1002049391187
+api_id = 33836080
+api_hash = "a5a02f0324ef4a8c767c6831aae67526"
+bot_token = "8455563091:AAE6F__oCeyXZ373rhLpKj5Er6--ZPf2494"
+auth_users = [1372134561]
+sudo_user = 1372134561
+log_channel = -1003574607642
+txt_channel = -1003574607642
 
 
