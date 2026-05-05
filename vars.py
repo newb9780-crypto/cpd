916b6f05da06d4d3421eee6e749723ff58610551
newb9780-crypto <newb9780@gmail.com>
2026-05-05 12:06:38 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
 api_id = 33836080
 api_hash = "a5a02f0324ef4a8c767c6831aae67526"
-bot_token = "8455563091:AAE6F__oCeyXZ373rhLpKj5Er6--ZPf2494"
-auth_users = [1372134561]
-sudo_user = 1372134561
-log_channel = -1003574607642
-txt_channel = -1003574607642
+bot_token = "8748256101:AAE5gox8iWkb0Q0D4bzv9NuOpT12vs5LNdo"
+auth_users = [1394117837]
+sudo_user = 1394117837
+log_channel = 
+txt_channel = 
 
 
